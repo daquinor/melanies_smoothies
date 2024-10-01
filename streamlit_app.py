6ebba01a5e4370bc191a1fe3708af8dd282cc3f8
daquinor <daniel.aquinorojas@albertahealthservices.ca>
2024-10-01 12:35:18 -0600

    fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,6 @@ if ingredients_list:
 #st.write(my_insert_stmt)
 #st.stop()
 
-
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
@@ -46,6 +45,8 @@ if time_to_insert:
 
     st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
-
-
+#New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
